01ce09c0c0b5e0b508ccbc9e3d19da379ae22097
Ted <ted_hodges@ymail.com>
2018-06-18 07:34:52 -0500

    copied working code from nytreact so we can have a starting point. feel free to remove.

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a className="navbar-brand" href="/">
+      NYT - React 
+    </a>
+  </nav>
+);
+
+export default Nav;
