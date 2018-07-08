0bcdb6b2bf996890d93bd6aee3e82551afe355f6
Ted <ted_hodges@ymail.com>
2018-07-08 12:07:06 -0500

    list item form is sending data and displaying on home page

--- models/book.js
+++ models/book.js
@@ -3,8 +3,12 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
+  category: {type: String, required: true },
   author: { type: String, required: true },
   synopsis: String,
+  image: { type: String, required: true },
+  location: { type: String, required: true },
+  price: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
 
