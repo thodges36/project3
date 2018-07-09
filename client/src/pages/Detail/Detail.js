c4819ab865944a7378461aaf2ee15071687e7c2a
Ted <ted_hodges@ymail.com>
2018-07-09 18:55:40 -0500

    adjusted Detail, ListItem, and Summary pages

--- client/src/pages/Detail/Detail.js
+++ client/src/pages/Detail/Detail.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import Header from "../../components/Header";
+
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 import API from "../../utils/API";
+import Calendar from 'react-calendar';
+
 
 class Detail extends Component {
   state = {
-    book: {}
+    book: {},
+    date: new Date()
   };
   // When this component mounts, grab the book with the _id of this.props.match.params.id
   // e.g. localhost:3000/books/599dcb67f0f16317844583fc
@@ -15,6 +19,9 @@ class Detail extends Component {
       .then(res => this.setState({ book: res.data }))
       .catch(err => console.log(err));
   }
+  
+  onChange = date => this.setState({ date })
+
 
   render() {
     return (
@@ -24,11 +31,7 @@ class Detail extends Component {
           <Col size="md-6">
             
             <img src={this.state.book.image} alt={this.state.book.title} class="img-large"></img>
-            
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-6">
+
             <article>
               <h1>
                 {this.state.book.title}
@@ -37,25 +40,68 @@ class Detail extends Component {
                 {this.state.book.synopsis}
               </p>
               <p>
-                Location: {this.state.book.location}
+              <i class="fas fa-map-marker-alt"></i>  {this.state.book.location}
               </p>
               <p>
-                Price: ${this.state.book.price}/hour
+              <i class="fas fa-dollar-sign"></i> {this.state.book.price}/hour
               </p>
+           
             </article>
+         
+            
+          </Col>
+  
+          <Col size="md-5">
+          <div>
+            <h4>
+              Rent this item
+            </h4>
+
+            <div class="alert alert-primary" role="alert">
+            This item is rentable for one day at a time!
+            </div>
+
+            <div>
+              <Calendar
+                onChange={this.onChange}
+                value={this.state.date}
+              />
+            </div>
+            <br />
+            <form>
+              <label>
+                Start Time (hour): 
+                <input
+                  name="start"
+                  type="text"
+               />
+              </label>
+              <br />
+              <label>
+                End Time (hour): 
+                <input
+                  name="end"
+                  type="text"
+                />
+              </label>
+            </form>
+
+           
+              
             <Link to="/summary">
-            <button type="button" class="btn btn-success">Rent Now</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Rent Now</button>
             </Link>
+
+            </div>
+
           </Col>
         </Row>
-        <Row>
-          {/* <Col size="md-2">
-            <Link to="/">← Back to Catalog</Link>
-          </Col> */}
-        </Row>
+ 
       </Container>
     );
   }
 }
+
+
 
 export default Detail;
